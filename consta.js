f678182b03011f1005b173f0361e45df8c3f42ce
saniya1276y7g <163450362+saniya1276y7g@users.noreply.github.com>
2025-01-16 12:30:27 +0530

    Add files via upload

---
+++ consta.js
@@ -0,0 +1,8 @@
+alert("hello")
+const profile={
+    name:"Arya",
+    posts:0,
+    following:245,
+    followers:245
+}
+console.log(profile);
---
+++ hello.js
@@ -0,0 +1,96 @@
+//sjsk
+//let  a=12;
+//let b=12;
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a%b=",a%b);//for remainder
+// console.log("a**b=",a**b);//double karel value
+// console.log("a/b=",a/b);
+
+// console.log("++a=",++a);  
+// console.log("a++=",a++);
+// console.log("--a=",--a);
+// console.log("a--=",a--);
+
+// console.log("a+=4",a+4);
+// console.log("a-=4",a-4);   //assignment operator
+// console.log("a%=4",a%4);
+// console.log("a**=4",a**4);
+
+// console.log("a==b",a==b); //comaprison
+// console.log("a!=b",a!=b);  // two values are not equal
+// console.log("a===b",a===b);  // compare data types          
+// console.log("a!==b",a!==b);  //not and double equal to
+// console.log("a>4",a>4);
+// console.log("a<=4",a<4);
+
+// let a=true;
+// let b=false;
+// console.log(a&&b);
+// console.log(a||b);  //logical operator
+// console.log(!b);
+
+// let mode="dark";
+// if(mode=="dark"){
+//     console.log(mode);
+// }
+
+// let age=16;
+// if(age>18){
+//     console.log("you are eligible")
+// }
+// else{
+//     console.log("you are not eligible")
+// }
+
+
+// let mode="pink";
+// if(mode=="light"){
+//     console.log(mode);
+// }
+// else if(mode=="dark"){
+//     console.log(mode);
+// }
+// else if(mode=="blue"){
+//     console.log(mode);
+// }
+// else{
+//     console.log("Invalid mode");
+// }
+
+
+// let a=4;
+// let b=3;
+// switch (a+b) {
+//     case (a-b):
+//         console.log(a-b);
+//         break;
+//     case (a+b):
+//         console.log(a+b);
+//         break;    
+//     default:
+//         break;
+// }
+
+// let num=prompt("enter a no");
+// if(num%5==0){
+//     console.log("this is correct no");  //5 multiple
+// }
+// else{
+//     console.log("not correct no");  //not 5 multiple
+// }
+
+// let num=prompt("enter a no");
+// if(num%2==0){
+//     console.log("this is even no");  //5 multiple
+// }
+// else{
+//     console.log("this is odd no");  //not 5 multiple
+// }
+
+
+let i=100;
+for(i==0;i>=0;i++){
+    console.log(i);
+}
